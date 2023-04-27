cccd9a6ad9bd1b611d16ecea2a131d90e65a1408
awj90 <dennis.awj@gmail.com>
2023-04-27 12:17:15 +0800

    fixed view0 bugs

--- src/main/java/sg/edu/nus/iss/fundtransferapp/models/Transfer.java
+++ src/main/java/sg/edu/nus/iss/fundtransferapp/models/Transfer.java
@@ -1,5 +1,6 @@
 package sg.edu.nus.iss.fundtransferapp.models;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 
 public class Transfer {
@@ -9,7 +10,10 @@ public class Transfer {
 
      @Size(min=10, max=10, message="Please enter a 10-digit account ID.")
      private String toAccountId;
+
+     @Min(value=1, message="Transfer amount must be more than 0.")
      private Double amount;  
+     
      private String comments;
 
     public String getFromAccountId() {
